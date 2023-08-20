c4917e28b9977841ca7ee291eead1ed64b1e6aa3
Jonathan Pillajo <jonathan.pillajo@hotmail.com>
2023-08-20 08:27:37 -0500

    primer commit

---
+++ app3.js
@@ -0,0 +1,18 @@
+
+console.log('Inicio de programa'); // 1
+
+setTimeout(() => {
+    console.log('Primer Timeout'); // 5
+}, 3000);
+
+setTimeout(() => {
+    console.log('Segundo Timeout'); // 2
+}, 0);
+
+setTimeout(() => {
+    console.log('Tercero Timeout'); // 3
+}, 0);
+
+
+
+console.log('Fin del programa'); // 4
